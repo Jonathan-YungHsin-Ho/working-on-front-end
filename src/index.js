5f6f0daa4a4506b270edeced14dfe4bd980888e8
Jonathan Ho <johnnyho82@gmail.com>
2020-03-13 23:25:02 -0400

    Set up React app

--- src/index.js
+++ src/index.js
@@ -2,9 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import ApolloClient, { InMemoryCache } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+	uri: process.env.REACT_APP_BACKEND,
+	cache,
+});
+
+ReactDOM.render(
+	<ApolloProvider client={client}>
+		<Router>
+			<App />
+		</Router>
+	</ApolloProvider>,
+	document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
